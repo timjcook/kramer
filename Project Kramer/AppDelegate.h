e055c1653936a36ab8ccf4ad53ab89b993297d5c
Tim Cook <tim@wakeless.net>
2014-03-16 14:32:40 +1100

    Initial Commit

---
+++ Project Kramer/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  Project Kramer
+//
+//  Created by Tim Cook on 16/03/2014.
+//  Copyright (c) 2014 Tim Cook. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
